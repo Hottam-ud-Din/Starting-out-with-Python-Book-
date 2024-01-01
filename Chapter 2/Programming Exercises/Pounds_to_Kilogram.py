78ee169ca52ca20c50116455fd86660babf16664
Hottam ud Din <141125858+Hottam-ud-Din@users.noreply.github.com>
2024-01-01 23:30:37 +0500

    Python file added

---
+++ Chapter 2/Programming Exercises/Pounds_to_Kilogram.py
@@ -0,0 +1,10 @@
+'''
+3. Pounds to Kilograms
+One pound is equivalent to 0.454 kilograms. Write a program that asks the user to enter 
+the mass of an object in pounds and then calculates and displays the mass of the object in 
+kilograms.
+'''
+
+mass_in_Pounds = int(input('Enter the mass of an object in pounds:'))
+mass_in_kg = mass_in_Pounds * 0.454
+print(f'Mass in pounds is {mass_in_Pounds}\n Mass in kg is {mass_in_kg}')
