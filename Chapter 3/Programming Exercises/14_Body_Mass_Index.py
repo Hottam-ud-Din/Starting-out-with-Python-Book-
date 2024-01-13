10a6e631ae83d8ce2d12a607ecce509de7bf33ec
Hottam ud Din <141125858+Hottam-ud-Din@users.noreply.github.com>
2024-01-13 22:43:12 +0500

    Python file added

---
+++ Chapter 3/Programming Exercises/14_Body_Mass_Index.py
@@ -0,0 +1,21 @@
+'''
+14. Body Mass Index
+Write a program that calculates and displays a person’s body mass index (BMI). The BMI
+is often used to determine whether a person is overweight or underweight for his or her 
+height. A person’s BMI is calculated with the following formula:
+
+                            BMI = weight * 703/height^2
+where weight is measured in pounds and height is measured in inches. The program should ask the user to enter his or her weight and height, then display the user’s BMI. The program should also display a message indicating whether the person has optimal weight, is underweight, or is overweight. A person’s weight is considered to be optimal if his or her BMI is between 18.5 and 25. If the BMI is less than 18.5, the person is considered to be underweight. If the BMI value is greater than 25, the person is considered to be overweight.
+'''
+weight = int(input('Enter your weight in pounds: '))
+height = int(input('Enter your height in inches: '))
+
+BMI = weight * 703/ height**2
+print(f'Your BMI is: {BMI:,.2f}')
+
+if BMI >= 18.5 and BMI <= 25:
+    print('Your weight is optimal')
+elif BMI < 18.5:
+    print('You are under weight')
+else:
+    print('You are overweight')
